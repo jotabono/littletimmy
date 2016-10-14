21fdaa4642a69688fd8ac2301870cf168f28a6e6
Xavier Pandis <xavier.pandis@gmail.com>
2016-10-14 20:50:50 +0200

    Fix frontend menu en login

--- src/main/webapp/content/js/menu.js
+++ src/main/webapp/content/js/menu.js
@@ -1,11 +1,22 @@
 $(document).ready(init);
 
 function init() {
+    var colorInitWebkit = "-webkit-linear-gradient(180deg, rgb(73, 255, 179) 20%, rgb(57, 148, 153) 100%)";
+    var colorInit = "linear-gradient(180deg, rgb(73, 255, 179) 20%, rgb(57, 148, 153) 100%)";
+
     $(window).scroll(function () {
-        if($(window).scrollTop()>=100){
-            $(".navbar").css('background','rgba(85, 172, 238, 0.8)');
-        } else {
-            $(".navbar").css('background','rgba(85, 172, 238, 1)');
+        var url = window.location.href.split('/#');
+        if (url[1] == "/login") {
+            $(".navbar").css('background', colorInit);
+            $(".navbar").css('background', colorInitWebkit);
+        }
+        else {
+            if ($(window).scrollTop() >= 100) {
+                $(".navbar").css('background', 'rgba(85, 172, 238, 0.8)');
+            } else {
+                $(".navbar").css('background', 'rgba(85, 172, 238, 1)');
+            }
         }
     });
 }
+
