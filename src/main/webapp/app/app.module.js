e02a54401a972cc542cebd001c94901d6f91030b
Javier Bono Parra <jotabono92@gmail.com>
2017-01-19 00:37:27 +0100

    Chat added to project

--- src/main/webapp/app/app.module.js
+++ src/main/webapp/app/app.module.js
@@ -21,6 +21,19 @@
             'xeditable',
             'ngMaterial'
         ])
+        .directive('myEnter', function () {
+            return function (scope, element, attrs) {
+                element.bind("keydown keypress", function (event) {
+                    if(event.which === 13) {
+                        scope.$apply(function (){
+                            scope.$eval(attrs.myEnter);
+                        });
+
+                        event.preventDefault();
+                    }
+                });
+            };
+        })
         .run(run);
 
     run.$inject = ['stateHandler', 'translationHandler'];
